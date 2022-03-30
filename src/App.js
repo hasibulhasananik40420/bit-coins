abc19efc3cd71e824f8d23ccab20b305b58a2430
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-03-30 15:14:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import CoinDetails from './components/CoinDetails/CoinDetails';
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+                  {/* 1.30 mimins */}
+
+
+      <Header></Header>
+      <div className='bg-gray-100'>
+      <Routes>
+       <Route path='/' element= {<Homepage></Homepage>}> </Route>
+       <Route path='/coins' element={<Coins></Coins>}></Route>
+       <Route path='/coin-details/:id' element={<CoinDetails></CoinDetails>}></Route>
+       <Route path='/contact' element={<Contact></Contact>}> </Route>
+       <Route path='/about' element={<About></About>}> </Route>
+      
+     </Routes>
+      <Footer></Footer>
+      </div>
+
     </div>
   );
 }
